dc897675b19b60fb95bb144c38a95f7eeaba4359
wmuth <98701865+wmuth@users.noreply.github.com>
2022-07-19 17:41:08 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.boobsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sboobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(838, 430);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(838, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // boobsToolStripMenuItem
+            // 
+            this.boobsToolStripMenuItem.Name = "boobsToolStripMenuItem";
+            this.boobsToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // sboobToolStripMenuItem
+            // 
+            this.sboobToolStripMenuItem.Name = "sboobToolStripMenuItem";
+            this.sboobToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.Filter = "Text documents|*.txt";
+            this.openFileDialog1.FilterIndex = 2;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.Filter = "Text documents|*txt";
+            this.saveFileDialog1.FilterIndex = 2;
+            this.saveFileDialog1.RestoreDirectory = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 454);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Notepad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem boobsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sboobToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class Form1 : Form
+    {
+        //Declare variables used several times later
+        private string filename = string.Empty;
+        private bool textChanged = false;
+
+        //Start of program
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Unnamed";
+        }
+
+        //NEWBUTTON
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //This, while clunky, creates better functionality than this.Close()
+            //Makes sure we are asked if we're sure if we have edited the file and doens't do anyhting
+            //if we tell it to cancel and doesn't close the program if we click no
+            var eventArgs = new FormClosingEventArgs(CloseReason.None, false);
+            Form1_FormClosing(null, eventArgs);
+            if (eventArgs.Cancel)
+            {
+                return;
+            }
+
+            //If we do want to go through with this then we reset everything to the default state
+            richTextBox1.Text = string.Empty;
+            Text = "Unnamed";
+            textChanged = false;
+            filename = string.Empty;
+        }
+
+        //OPENBUTTON
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Same as function above
+            var eventArgs = new FormClosingEventArgs(CloseReason.None, false);
+            Form1_FormClosing(null, eventArgs);
+            if (eventArgs.Cancel)
+            {
+                return;
+            }
+
+            //We open a file dialog and if one is selected then load it
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textChanged = false;
+                richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+                filename = openFileDialog1.FileName;
+                Text = openFileDialog1.SafeFileName;
+            }
+        }
+
+        //SAVEBUTTON
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        //Save, has its own function rather than just being save button since it is called elsewhere
+        private void SaveFile()
+        {
+            //Remove the star and set to false
+            Text = Text.Remove(Text.Length - 1);
+            textChanged = false;
+
+            //If we don't already have a file, we're editing
+            if (string.IsNullOrEmpty(filename))
+            {
+                SaveAs();
+            }
+
+            //If we were already working on a file then just save our text to it
+            else
+            {
+                File.WriteAllText(filename, richTextBox1.Text);
+            }
+        }
+
+        //SAVEASBUTTON
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        private void SaveAs()
+        {
+            //We show a dialog and when a file is selected to be saved we put all our text there
+            //and update all the variables to point to that file
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filename = saveFileDialog1.FileName;
+                Text = filename;
+                string path = Path.GetFullPath(saveFileDialog1.FileName);
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(richTextBox1.Text);
+                }
+            }
+        }
+
+        //EXITBUTTON
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Add star to show text edited since last save, used in parts of the program to know
+        //if we have edited the text
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!textChanged)
+            {
+                Text = Text + "*";
+            }
+            textChanged = true;
+        }
+
+        //Tell user their changes are not saved and save if requested
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (textChanged)
+            {
+                DialogResult dialogResult = MessageBox.Show("You have unsaved edits. Save?", "Save?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    SaveFile();
+                }
+                if (dialogResult == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+                else { }
+            }
+        }
+    }
+}
